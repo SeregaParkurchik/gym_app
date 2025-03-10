9501434328bb36575c977fa6b700537cf460033b
Sergey_Krasnow <serejjja3@yandex.ru>
2025-03-10 12:35:48 +0300

    proto1

--- internal/server/main.go
+++ internal/server/main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+
+	desc "github.com/SeregaParkurchik/gym_app/pkg/proto_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 const grpcPort = 50051
@@ -11,5 +17,18 @@ type server struct {
 }
 
 func main() {
-	fmt.Println("serega")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterNoteV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
